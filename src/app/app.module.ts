a9f2683cc766e8d5c2f6f90ee942fd5201b4df7c
alexjjose <alexjjosea7@gmail.com>
2022-12-07 20:22:50 +0530

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddtodoComponent } from './addtodo/addtodo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewalltodoComponent } from './viewalltodo/viewalltodo.component';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+const myroutes=[
+  {path:"",component:AddtodoComponent},
+  {path:"view",component:ViewalltodoComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtodoComponent,
+    NavbarComponent,
+    ViewalltodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes),
+    HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
